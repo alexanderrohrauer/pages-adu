29fcac3c6f35ae693a16913bb8e0250fd605b372
alexanderrohrauer <alex.rohrauer@outlook.de>
2026-08-02 12:19:44 +0200

    corrected docs-unit Dockerfile

--- setup-host.sh
+++ setup-host.sh
@@ -5,7 +5,7 @@ mkdir config
 wget https://raw.githubusercontent.com/alexanderrohrauer/pages-adu/refs/heads/main/deployment-targets/docker/config/config.js -O ./config/config.js
 wget https://raw.githubusercontent.com/alexanderrohrauer/pages-adu/refs/heads/main/deployment-targets/docker/docker-compose.yml
 DOCKER_GROUP_GID=$(stat -c '%g' /var/run/docker.sock)
-echo "export DOCKER_GROUP_GID=$DOCKER_GROUP_GID;" >> "$HOME/.bash_profile"
+echo "export DOCKER_GROUP_GID=$DOCKER_GROUP_GID;" >> "$HOME/.bashrc"
 export DOCKER_GROUP_GID=$DOCKER_GROUP_GID
 touch .env
 nano .env
